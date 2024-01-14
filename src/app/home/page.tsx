724d5c53cfa1788c9ed8cbc7a6a7f75a356514d8
haeun kim <ckhe1215@gmail.com>
2024-01-15 03:44:52 +0900

    상위에서 테스트

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -3,12 +3,16 @@
 import { FC } from "react";
 import UpcomingStockSection from "@/components/home/UpcomingStockSection";
 import EndedStockSection from "@/components/home/EndedStockSection";
+import getDateAfter20BusinessDays from "@/util/getDateAfter20BusinessDays";
 
 const HomePage: FC = () => {
+  const after20BusiDate = getDateAfter20BusinessDays();
+
   return (
     <>
-      <UpcomingStockSection />
-      <EndedStockSection />
+      <p>{after20BusiDate.toLocaleString()}</p>
+      {/* <UpcomingStockSection /> */}
+      {/* <EndedStockSection /> */}
     </>
   );
 };
