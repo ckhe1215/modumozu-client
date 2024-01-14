02ab4e20fafbeb7513538d3e2c9c24cd8b5cb60d
haeun kim <ckhe1215@gmail.com>
2024-01-14 21:23:10 +0900

    transpilePackages 테스트

--- next.config.js
+++ next.config.js
@@ -6,9 +6,7 @@ const nextConfig = {
   images: {
     domains: ["d2u3dcdbebyaiu.cloudfront.net", "teamblindstatics.s3.ap-northeast-1.amazonaws.com"],
   },
-  experimental: {
-    esmExternals: true,
-  },
+  transpilePackages: ["korean-business-day"],
 };
 
 module.exports = nextConfig;
