eb3a7706b5d4559abda11b1fc38d1ce83ecd70d9
haeun kim <ckhe1215@gmail.com>
2024-01-15 03:17:22 +0900

    suppressHydrationWarning 테스트

--- src/components/home/modal/OpenAccountFull.tsx
+++ src/components/home/modal/OpenAccountFull.tsx
@@ -22,43 +22,45 @@ const OpenAccountFull: FC<OpenAccountFullProps> = (props) => {
   const after20BusiDate = getDateAfter20BusinessDays();
   // const after20BusiDateNormal = dayjs().add(26, "day");
   return (
-    <FullScreenModal visible={agentId > 0} setInvisible={handleClose}>
-      <FullScreenModalDescription>
-        <h2>
-          <span>{getBankName(agentId)}</span> 계좌를 개설하면
-          <br /> 다음 청약을 넣을 수 없어요.
-        </h2>
-        <p>
-          유안타증권, 한화투자증권, 교보증권 외 19개 증권사들은 20일 이내에 계좌를 개설한 내역이 있을 경우 증권사 계좌
-          개설을 막고 있어요.
-        </p>
-      </FullScreenModalDescription>
-      {stockList
-        .filter((item) => new Date(item.proposal.needAt) < after20BusiDate)
-        .filter((item) => item.remainAgents.length === 0 && !item.nonRemainAgents.includes(agentId))
-        .filter((item) => !item.nonRemainAgents.some((agent) => limitlessAgent.includes(agent)))
-        .map((data) => (
-          <UpcomingStockItem key={data.id}>
-            <UpcomingStock.status startDate={data.offerBeginAt} endDate={data.offerEndAt} />
-            <UpcomingStock.cardWrap
-              id={data.id}
-              title={data.name}
-              love={data.pinned}
-              category={data.category}
-              account={data.remainAgents}
-              nonRemainAccounts={data.nonRemainAgents}
-              price={[data.minDesiredOfferPrice, data.maxDesiredOfferPrice]}
-              cardType={data.proposal.cardType}
-              proposalAgent={data.proposal.agentId}
-              proposalEndDate={data.proposal.needAt}
-              onClick={() => window.open(getStoreUrl(data.proposal.agentId))}
-            />
-          </UpcomingStockItem>
-        ))}
-      <BottomButton width="100%" $font="BUTTON1_SEMIBOLD" onClick={() => window.open(getStoreUrl(agentId))}>
-        계좌 개설
-      </BottomButton>
-    </FullScreenModal>
+    <div suppressHydrationWarning>
+      <FullScreenModal visible={agentId > 0} setInvisible={handleClose}>
+        <FullScreenModalDescription>
+          <h2>
+            <span>{getBankName(agentId)}</span> 계좌를 개설하면
+            <br /> 다음 청약을 넣을 수 없어요.
+          </h2>
+          <p>
+            유안타증권, 한화투자증권, 교보증권 외 19개 증권사들은 20일 이내에 계좌를 개설한 내역이 있을 경우 증권사 계좌
+            개설을 막고 있어요.
+          </p>
+        </FullScreenModalDescription>
+        {stockList
+          .filter((item) => new Date(item.proposal.needAt) < after20BusiDate)
+          .filter((item) => item.remainAgents.length === 0 && !item.nonRemainAgents.includes(agentId))
+          .filter((item) => !item.nonRemainAgents.some((agent) => limitlessAgent.includes(agent)))
+          .map((data) => (
+            <UpcomingStockItem key={data.id}>
+              <UpcomingStock.status startDate={data.offerBeginAt} endDate={data.offerEndAt} />
+              <UpcomingStock.cardWrap
+                id={data.id}
+                title={data.name}
+                love={data.pinned}
+                category={data.category}
+                account={data.remainAgents}
+                nonRemainAccounts={data.nonRemainAgents}
+                price={[data.minDesiredOfferPrice, data.maxDesiredOfferPrice]}
+                cardType={data.proposal.cardType}
+                proposalAgent={data.proposal.agentId}
+                proposalEndDate={data.proposal.needAt}
+                onClick={() => window.open(getStoreUrl(data.proposal.agentId))}
+              />
+            </UpcomingStockItem>
+          ))}
+        <BottomButton width="100%" $font="BUTTON1_SEMIBOLD" onClick={() => window.open(getStoreUrl(agentId))}>
+          계좌 개설
+        </BottomButton>
+      </FullScreenModal>
+    </div>
   );
 };
 
