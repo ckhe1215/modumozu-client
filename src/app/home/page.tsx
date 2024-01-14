2575ffb49b22353eac0ff04f92f457d84055aa52
haeun kim <ckhe1215@gmail.com>
2024-01-15 03:51:47 +0900

    상위에서 useEffect

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,16 +1,19 @@
 "use client";
 
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 import UpcomingStockSection from "@/components/home/UpcomingStockSection";
 import EndedStockSection from "@/components/home/EndedStockSection";
 import getDateAfter20BusinessDays from "@/util/getDateAfter20BusinessDays";
 
 const HomePage: FC = () => {
-  const after20BusiDate = getDateAfter20BusinessDays();
+  const [after20BusiDate, setAfter20BusiDate] = useState<Date>();
+  useEffect(() => {
+    setAfter20BusiDate(getDateAfter20BusinessDays());
+  }, []);
 
   return (
     <>
-      <p>{after20BusiDate.toLocaleString()}</p>
+      {after20BusiDate ? <p>{after20BusiDate.toLocaleString()}</p> : <p>hello</p>}
       {/* <UpcomingStockSection /> */}
       {/* <EndedStockSection /> */}
     </>
